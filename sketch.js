8b340476abed75fa5bbc23ad88ad1e6ff777e266
BenjaminHerfray <74919628+BenjaminHerfray@users.noreply.github.com>
2021-01-10 21:10:58 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,8 +13,11 @@
 const c = 30;
 const G = 2;
 const dt = 0.1;
+const period = 50;
+const error = 2.1;
 
 let m87;
+let star1;
 
 const particles = [];
 let start, end;
@@ -27,9 +30,12 @@ function setup() {
   end = height / 2 - m87.rs * 2.6;
 
   for (let y = 0; y < start; y += 10) {
-    particles.push(new Photon(width - 20, y));
+    particles.push(new Photon(width - 20, y, -c, 0));
   }
+  star1 = new Star(50, 50, 10, c/6, 0);
 }
+
+let i = 0;
 
 function draw() {
   background(255);
@@ -38,11 +44,23 @@ function draw() {
   strokeWeight(1);
   line(0, start, width, start);
   line(0, end, width, end);
-
+  
+  if (i === period) {
+    particles.push(new Photon(star1.position.x +            star1.radius, star1.position.y, c, 0));
+    particles.push(new Photon(star1.position.x -  star1.radius, star1.position.y, -c, 0));
+    particles.push(new Photon(star1.position.x,  star1.position.y + star1.radius, 0, c));
+    particles.push(new Photon(star1.position.x,    star1.position.y - star1.radius, 0, -c));
+    i = 0;
+  } else {
+    i++;
+  }
   for (let p of particles) {
     m87.pull(p);
     p.update();
     p.show();
   }
+  m87.attract(star1);
+  star1.move();
+  star1.show();
   m87.show();
 }
